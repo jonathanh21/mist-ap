787f0e5f023cc273d70be37d042e7c213fc3a768
Valentin Porta <valentinporta93@gmail.com>
2021-06-08 12:12:39 -0300

    agregue mi componente

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import Julian from './components/Julian';
 import './App.css';
+import Valentin from './components/Valentin';
 
 function App() {
   return (
     <div className='App'>
       <Julian />
+      <Valentin />
     </div>
   );
 }
