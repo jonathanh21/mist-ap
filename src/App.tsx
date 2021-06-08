455ec345bce0d42c21ba7f5f29a27f1394eadbf9
unknown <viterr@gmail.com>
2021-06-08 12:08:02 -0300

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import Julian from './Julian';
+import Julian from './components/Julian';
+import Victor from './components/Victor';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Julian />
+      <Victor />
     </div>
   );
 }
---
+++ src/components/Victor.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Victor() {
+  return <div>hola GIT</div>;
+}
+
+export default Victor;
