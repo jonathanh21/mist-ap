a2d4ca29b0a0e9583c603d2b33e4126b3e21f85a
jonathanh21 <jhonathan.herrerao@gmail.com>
2021-06-08 14:24:58 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import Julian from './components/Julian';
-import Valentin from './components/Valentin';
-import Probando from './components/Probando';
-import Jonathan from './components/jonathan';
+
 import './App.css';
 function App() {
   return (
     <div className='App'>
-      <Julian />
-      <Valentin />
-      <Probando />
-      <Jonathan />
     </div>
   );
 }
