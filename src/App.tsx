c385f352a1425aa29665b5486430fc2935ac20b7
rodriguezjulianf <julipinchasl60@gmail.com>
2021-06-08 12:08:38 -0300

    ramaJulian

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Julian from './components/Julian';
+import Probando from './components/Probando';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Julian />
+      <Probando />
     </div>
   );
 }
---
+++ src/components/Probando.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Probando() {
+  return <div>pruebo</div>;
+}
+
+export default Probando;
