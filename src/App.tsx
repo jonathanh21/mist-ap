0ae81efb1af9ece0e3e55b463f0947afb128dc6e
jonathanh21 <jhonathan.herrerao@gmail.com>
2021-06-08 18:48:18 -0500

    Add Select Input y Rxjs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import InputExpDate from "./components/InputExpDate.component"
 import './App.css';
+import { FormCrearCuenta } from "./components/FormCrearCuenta"
+
 function App() {
   return (
     <div className='App'>
+      <FormCrearCuenta />
+      <InputExpDate />
     </div>
   );
 }
