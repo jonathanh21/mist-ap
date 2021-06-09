5460b36bd1dfafbb4402c47d0b718cc898d6ca5c
unknown <viterr@gmail.com>
2021-06-09 20:29:02 -0300

    feature/SelectInput & TitleText & StepsText

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import StepsText from './components/StepsText.component';
-import TitleText from './components/TitleText.component';
+
 import { FormCrearCuenta } from './pages/CrearCuenta.page/components/FormCrearCuenta.component';
 
 function App() {
   return (
     <div className='App'>
-      <StepsText text='Paso 1 de 2' />
-      <TitleText text='Viculación legal' />
       <FormCrearCuenta />
     </div>
   );
